85df6ebccec6bbd43e87f6976bacd2450221ef1f
Stefan Schneider <stefan@netshake.com>
2021-11-19 23:52:16 +0100

    test: :white_check_mark: Added test for field validation error

--- ui/src/components/select/__tests__/QSelect.spec.js
+++ ui/src/components/select/__tests__/QSelect.spec.js
@@ -35,6 +35,56 @@ describe('QSelect', () => {
           .should('eq', 'notname')
       })
     })
+
+    describe('(prop): virtual-scroll-horizontal', () => {
+      it('???', () => {
+        mount(SelectWrapperForm, {
+          attrs: {
+            'virtual-scroll-horizontal': true
+          }
+        })
+        // eslint-disable-next-line no-unused-expressions
+        expect(true).to.be.true
+      })
+
+      it('???', () => {
+        mount(SelectWrapperForm, {
+          attrs: {
+            'virtual-scroll-horizontal': false
+          }
+        })
+        // eslint-disable-next-line no-unused-expressions
+        expect(true).to.be.true
+      })
+    })
+
+    describe('(prop): error', () => {
+      it('should show a field validation error', () => {
+        mount(SelectWrapperForm, {
+          attrs: {
+            error: true
+          }
+        })
+
+        cy.get('.text-negative .q-icon')
+          .should('exist')
+          .should('be.visible')
+
+        cy.get('.text-negative .q-icon')
+          .should('contain', 'error')
+      })
+
+      it('should not show a field validation error', () => {
+        mount(SelectWrapperForm, {
+          attrs: {
+            error: false
+          }
+        })
+
+        cy.get('.text-negative .q-icon')
+          .should('not.exist')
+      })
+    })
   })
   // Model tests
 
